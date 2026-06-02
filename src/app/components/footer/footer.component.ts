86b9f2d18d4756241de24478a5837d71e4523d11
Dario <dario.gilles@outlook.com>
2026-06-02 13:37:04 +0200

    footer update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SettingsService, AppSettings } from '../../services/settings.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear = new Date().getFullYear();
+  settingsService = inject(SettingsService);
+  settings: AppSettings | null = null;
 
+  ngOnInit() {
+    this.settingsService.getSettings().subscribe(s => {
+      if (s) {
+        this.settings = s;
+      }
+    });
+  }
 }
